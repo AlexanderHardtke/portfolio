7263a06a0f9727e90eab46007b521ee6868a9eea
Alexander Hardtke <5yl3kz@googlemail.com>
2024-11-26 18:35:30 +0100

    Footer complete

--- src/app/portfolio/project/project.component.ts
+++ src/app/portfolio/project/project.component.ts
@@ -13,7 +13,8 @@ export class ProjectComponent {
   projects = [
     {
       name: "Sharkie",
-      img:"project_sharkie.png",
+      imgDevice:"project_sharkie.png",
+      imgBlank:"sharkie_desktop.png",
       descriptionEng: "A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.",
       descriptionGer: "Ein einfaches Jump-and-Run-Spiel basierend auf einem objektorientierten Ansatz. Hilf Sharkie dabei, Münzen und Giftflaschen zu finden, um gegen den Killerwal zu kämpfen.",
       usedTech: "JavaScript | HTML | CSS",
@@ -22,7 +23,8 @@ export class ProjectComponent {
     },
     {
       name: "Pokedex",
-      img:"project_pokedex.png",
+      imgDevice:"project_pokedex.png",
+      imgBlank:"pokedex_desktop.png",
       descriptionEng: "Based on the PokéAPI a simple library that provides and catalogues pokemon information.",
       descriptionGer: "Basierend auf der PokéAPI. Eine einfache Bibliothek die Pokemondaten bereitstellt und katalogisiert.",
       usedTech: "JavaScript | HTML | CSS | API",
@@ -31,7 +33,8 @@ export class ProjectComponent {
     },
     {
       name: "Join",
-      img:"project_sharkie.png",
+      imgDevice:"project_sharkie.png",
+      imgBlank:"sharkie_desktop.png",
       descriptionEng: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
       descriptionGer: "Taskmanager, inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mithilfe von Drag-and-Drop-Funktionen und weisen Sie Benutzer und Kategorien zu.",
       usedTech: "JavaScript | HTML | CSS | Firebase",
@@ -40,7 +43,8 @@ export class ProjectComponent {
     },
     {
       name: "DABubble",
-      img:"#",
+      imgDevice:"#",
+      imgBlank:"sharkie_desktop.png",
       descriptionEng: "description english",
       descriptionGer: "Beschreibung deutsch",
       usedTech: "GitHub",
