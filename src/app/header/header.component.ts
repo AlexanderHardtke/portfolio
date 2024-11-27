0b383f6663d88b5c22cabe8665979761fd67ab7e
Alexander Hardtke <5yl3kz@googlemail.com>
2024-11-27 11:28:23 +0100

    language buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  lang: "en" | "ger";
 
+  constructor() {
+    this.lang = "en";
+  }
+
+  setLang(lang: string) {
+    if (lang == "ger" || lang == "en") {
+      this.lang = lang;
+    }
+      
+  }
 }
