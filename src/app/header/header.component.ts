60afa886b153287be9544b2835fa5c2096cefbc5
Alexander Hardtke <5yl3kz@googlemail.com>
2024-12-05 08:50:47 +0100

    header responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule} from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Renderer2 } from '@angular/core';
 import { PortfoliodataService } from '../portfoliodata.service';
 
 @Component({
@@ -11,11 +11,23 @@ import { PortfoliodataService } from '../portfoliodata.service';
 })
 export class HeaderComponent {
   portData = inject(PortfoliodataService);
+  isMenuOpen = false;
+
+  constructor(private renderer: Renderer2) { }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      this.renderer.removeStyle(document.body, 'overflow');
+    }
+  }
 
   setLang(lang: string) {
     if (lang == "ger" || lang == "en") {
       this.portData.lang = lang;
     }
-      
+
   }
 }
