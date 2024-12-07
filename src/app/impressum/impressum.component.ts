d42bbabed989d48a2f08cd10a32bdf9178a3aec0
Alexander Hardtke <5yl3kz@googlemail.com>
2024-12-07 16:30:40 +0100

    impressum english

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -12,9 +12,16 @@ export class ImpressumComponent {
   portData = inject(PortfoliodataService);
 
   /**
-     * displays the main content on the website
-     */
-  goToMainContent() {
-    this.portData.page = "index"
+   * displays the main content for the website and moves to the section selected by the user
+   * 
+   * @param href the section selected by the user
+   */
+  goToMainContent(href: string) {
+    if (this.portData.page == 'index') {
+      window.location.hash = href;
+    } else {
+      this.portData.page = 'index';
+      setTimeout(() => window.location.hash = href, 200);
+    }
   }
 }
